4504c75b3c021825cafe012147fcfc95f0119d0f
長島 <nf41deg3a56bv@gmail.com>
2022-07-27 09:14:53 +0000

    saiteisyutu

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create','Admin\NewsController@add');
 });
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create','Admin\ProfileController@add');
+    Route::get('admin/profile/edit','Admin\ProfileController@edit');
+});
